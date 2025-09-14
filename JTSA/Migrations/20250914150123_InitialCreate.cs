4eefbf8a2f8af2b7266672d9d04eb36341b2e32b
hirenjak <hirenjak@gmail.com>
2025-09-15 00:05:15 +0900

    #5 DBMigrationを適用（既存DBは破棄の必要あり）

---
+++ JTSA/Migrations/20250914150123_InitialCreate.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JTSA.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "M_CategoryList",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    BoxArtUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    CountSelected = table.Column<int>(type: "INTEGER", nullable: false),
+                    SortNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastUseDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_M_CategoryList", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "M_FriendList",
+                columns: table => new
+                {
+                    BroadcastId = table.Column<string>(type: "TEXT", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: false),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    CountSelected = table.Column<int>(type: "INTEGER", nullable: false),
+                    SortNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastUseDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_M_FriendList", x => x.BroadcastId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "M_SettingList",
+                columns: table => new
+                {
+                    Name = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Value = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_M_SettingList", x => x.Name);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "M_TitleTagList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DisplayName = table.Column<string>(type: "TEXT", nullable: false),
+                    CountSelected = table.Column<int>(type: "INTEGER", nullable: false),
+                    SortNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastUseDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_M_TitleTagList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "M_TitleTextList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryId = table.Column<string>(type: "TEXT", nullable: false),
+                    CategoryName = table.Column<string>(type: "TEXT", nullable: false),
+                    CountSelected = table.Column<int>(type: "INTEGER", nullable: false),
+                    SortNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "INTEGER", nullable: false),
+                    LastUseDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDateTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_M_TitleTextList", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "M_CategoryList");
+
+            migrationBuilder.DropTable(
+                name: "M_FriendList");
+
+            migrationBuilder.DropTable(
+                name: "M_SettingList");
+
+            migrationBuilder.DropTable(
+                name: "M_TitleTagList");
+
+            migrationBuilder.DropTable(
+                name: "M_TitleTextList");
+        }
+    }
+}
