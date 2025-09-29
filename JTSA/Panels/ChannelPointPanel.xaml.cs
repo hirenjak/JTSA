af3ec58a05c022d9fec6325775f0f1dc75ac9f97
Xipher <yukino.konata@gmail.com>
2025-09-30 04:52:25 +0900

    メイン処理の画面更新一覧に。チャンネルポイントのタブをのリフレッシュを追加

--- JTSA/Panels/ChannelPointPanel.xaml.cs
+++ JTSA/Panels/ChannelPointPanel.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JTSA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,25 @@ namespace JTSA.Panels
     /// </summary>
     public partial class ChannelPointPanel : UserControl
     {
+        MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
         public ChannelPointPanel()
         {
             InitializeComponent();
+        }
+        public void ReloadChannnelPoint()
+        {
+            mainWindow.AppLogPanel.AddProcessLog(GetType().Name, "チャンネルポイントリスト再読み込み", "処理開始");
+
+            // DB接続と初期化処理
+            using var db = new AppDbContext();
+            ChannelPointGetStatus.Text = "hi!!!";
+
+            // データの取得
+            var records = M_TitleTag.SelectAllOrderbyLastUser();
+
+            // 画面データ入れ換え処理
+
+            mainWindow.AppLogPanel.AddProcessLog(GetType().Name, "チャンネルポイントリスト再読み込み", "処理終了");
         }
     }
 }
